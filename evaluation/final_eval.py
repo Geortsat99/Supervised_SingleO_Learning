fa9594e6f695c4bb88a9da2d10d21e6a65e93960
Geortsat99 <geortsat@outlook.com>
2026-02-28 21:50:52 +0200

    Commit 003

--- evaluation/final_eval.py
+++ evaluation/final_eval.py
@@ -1,14 +1,38 @@
-
+#IMPORTS
+from sklearn.preprocessing import MinMaxScaler
+import numpy as np
 
 
 
 
 
 class Evaluation:
-    def __init__(self, results_df,sort_metric,time_gravity):
-        self.results_df = results_df
+    def __init__(self, all_results_df, sort_metric,time_gravity=0):
+        self.all_results_df = all_results_df
         self.sort_metric = sort_metric
         self.time_gravity = time_gravity
 
     def evaluate(self):
-        self.results_df['custom_metric']= -self.time_gravity * (self.results_df['fit_time'] + self.results_df['score_time']) +
+        all_res_df=self.all_results_df.copy()
+        all_res_df['logged_total_time']=np.log(all_res_df['fit_time']+all_res_df['score_time'])
+        scaler = MinMaxScaler()
+        all_res_df['scaled_time'] = scaler.fit_transform(all_res_df[["logged_total_time"]])
+        all_res_df['time_grade']=1-all_res_df['scaled_time']
+        all_res_df['scaled_metric']=scaler.fit_transform(all_res_df[[f'test_{self.sort_metric}']])
+        if all_res_df[f'test_{self.sort_metric}'].mean()<0:
+            all_res_df['metric_grade']=1-all_res_df['scaled_metric']
+        all_res_df['final_score']=self.time_gravity*all_res_df["time_grade"]+(1-self.time_gravity)*all_res_df["metric_grade"]
+        return all_res_df
+
+    def get_best_model_name(self):
+        res_df=self.evaluate()
+        best_model_name=res_df.loc[res_df['final_score'].max()]['model_name']
+        return best_model_name
+
+    def get_best_model_params(self):
+        res_df=self.evaluate()
+        best_model_params=res_df.loc[res_df['final_score'].max()]['params']
+        return best_model_params
+
+
+
--- training_cv/cross_validation.py
+++ training_cv/cross_validation.py
@@ -4,7 +4,8 @@ from sklearn.model_selection import cross_validate
 
 
 class CVResults:
-    def __init__(self, pipe, X, Y, k_settings, fit_metrics, config_params):
+    def __init__(self, model_name, pipe, X, Y, k_settings, fit_metrics, config_params):
+        self.model_name = model_name
         self.pipe = pipe
         self.k_settings = k_settings
         self.fit_metrics = fit_metrics
@@ -17,5 +18,7 @@ class CVResults:
         pipe.set_params(**self.config_params)
         results_cv = cross_validate(pipe, self.X, self.Y, cv=self.k_settings, scoring=self.fit_metrics)
         results_df=pd.DataFrame(results_cv)
-        results_df=results_df.mean()
-        return results_df
+        result_df = results_df.mean().to_frame().T
+        result_df['parameters']=self.config_params
+        result_df['model_name']=self.model_name
+        return result_df
